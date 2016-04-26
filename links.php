32f10c302116ede404329a5e80f87dc2a6359b36
Reed Rizzo <reedling78@gmail.com>
2016-04-25 21:33:00 -0500

    Added more PDF's

--- links.php
+++ links.php
@@ -7,8 +7,14 @@
   
   <h2>PDF's</h2>
   <p>
-    <a href="media/sample1.pdf" class="btn btn-default" aria-label="Sales Report" download="SalesReport">
-      <span class="glyphicon glyphicon-save-file" aria-hidden="true"></span>Sales Report (PDF)
+    <a href="media/sample1.pdf" class="btn btn-default" aria-label="Sales Report 2013" download="SalesReport2013">
+      <span class="glyphicon glyphicon-save-file" aria-hidden="true"></span>Sales Report 2013
+    </a>
+    <a href="media/sample2.pdf" class="btn btn-default" aria-label="Sales Report 2014" download="SalesReport2014">
+      <span class="glyphicon glyphicon-save-file" aria-hidden="true"></span>Sales Report 2014
+    </a>
+    <a href="media/sample3.pdf" class="btn btn-default" aria-label="Sales Report 2015" download="SalesReport2015">
+      <span class="glyphicon glyphicon-save-file" aria-hidden="true"></span>Sales Report 2015
     </a>
     <code>
       <pre>
