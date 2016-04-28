c7151775f30fcc5bd2d011ca509d0f733359adba
Reed Rizzo <reedling78@gmail.com>
2016-04-27 23:24:20 -0500

    Title Change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php include_once("includes/header.php"); ?>
 
 <div class="jumbotron">
-  <h1>How to get report junkies there fix. </h1>
+  <h1>Google Tag Manager</h1>
   <p>Google Tag Manager keeps track of a set of tags and tag-firing triggers that define when those tags should be made available on your site. When a user visits the site, the most up-to-date tag configuration is sent over to the end-user's browser with instructions for which tags should fire.</p>
   <p>
     <a class="btn btn-lg btn-primary" href="#" role="button">Just another link &raquo;</a>
